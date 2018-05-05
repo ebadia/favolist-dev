271d09c5fb0c91deb93336caa74d87f62affd9e3
ebadia <enric.badia@gmail.com>
2018-05-05 18:55:08 +0200

    fix: added update item dto

--- src/controllers/items/dto/update-item.dto.ts
+++ src/controllers/items/dto/update-item.dto.ts
@@ -1,9 +1,10 @@
 import { IsString, IsInt, IsNumber, IsArray, IsOptional, IsNotEmpty } from 'class-validator'
+import { CreateProductOrderDto } from '../../products/dto/create-product-order.dto'
 
 export class UpdateItemDto {
   @IsOptional() @IsInt() readonly id: number
   @IsOptional() @IsInt() readonly quantity: number
   @IsOptional() @IsString() readonly status: string
-  @IsOptional() @IsInt() readonly product: number
+  readonly product: CreateProductOrderDto
   @IsOptional() @IsInt() readonly order: number
 }
