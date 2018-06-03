3b702ed1a3483ec4872edd22046db33cbab3d79e
ebadia <enric.badia@gmail.com>
2018-06-03 10:52:07 +0200

    cambiado campo de control de stock en availables

--- src/controllers/availables/dto/update-available.dto.ts
+++ src/controllers/availables/dto/update-available.dto.ts
@@ -1,11 +1,31 @@
-import { IsString, IsInt, IsNumber, IsArray, IsOptional, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsInt,
+  IsNumber,
+  IsArray,
+  IsOptional,
+  IsNotEmpty
+} from 'class-validator'
 
 import { AssignProductDto } from '../../products/dto/assign-product.dto'
 
 export class UpdateAvailableDto {
-  @IsOptional() @IsInt() readonly id: number
-  @IsOptional() @IsString() readonly day: string
-  @IsOptional() @IsNumber() readonly stock: number
-  @IsOptional() @IsNumber() readonly price: number
-  @IsOptional() @IsNotEmpty() readonly product: AssignProductDto
+  @IsOptional()
+  @IsInt()
+  readonly id: number
+  @IsOptional()
+  @IsString()
+  readonly day: string
+  @IsOptional()
+  @IsNumber()
+  readonly availableStock: number
+  @IsOptional()
+  @IsNumber()
+  readonly availableStockOut: number
+  @IsOptional()
+  @IsNumber()
+  readonly price: number
+  @IsOptional()
+  @IsNotEmpty()
+  readonly product: AssignProductDto
 }
